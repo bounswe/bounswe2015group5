186e1873578dd0035edbcc152331968bdc4d37f6
Hakan Şahin <hakansahin993@gmail.com>
2015-05-03 21:21:34 +0300

    binaryMinus and testBinaryMinus methods added. Gitignore file created.

--- src/MathematicalOperations.java
+++ src/MathematicalOperations.java
@@ -1,7 +1,9 @@
 /**
  * Created by Can Guler on 03.05.2015.
  */
+
 public class MathematicalOperations {
+
     /**
      * Performs addition of x and y.
      * Added by Can Guler.
@@ -11,5 +13,16 @@ public class MathematicalOperations {
      */
     public static int binaryPlus(int x, int y) {
         return x+y;
+    }
+
+    /**
+     * Performs subtraction of y from x.
+     * Added by Hakan Şahin.
+     * @param x First operand
+     * @param y Second operand
+     * @return x minus y
+     */
+    public static int binaryMinus(int x, int y) {
+        return x-y;
     }
 }
--- src/TestMathematicalOperations.java
+++ src/TestMathematicalOperations.java
@@ -7,11 +7,17 @@ import static org.junit.Assert.*;
  */
 public class TestMathematicalOperations {
 
-
     @Test
     public void testBinaryPlus() throws Exception {
         assertEquals(1, MathematicalOperations.binaryPlus(3, -2));
         assertEquals(0, MathematicalOperations.binaryPlus(3, -3));
         assertEquals(1355, MathematicalOperations.binaryPlus(1333, 22));
+    }
+
+    @Test
+    public void testBinaryMinus() throws Exception {
+        assertEquals(3, MathematicalOperations.binaryMinus(15, 12));
+        assertEquals(-119, MathematicalOperations.binaryMinus(-72, 47));
+        assertEquals(64, MathematicalOperations.binaryMinus(-23, -87));
     }
 }
