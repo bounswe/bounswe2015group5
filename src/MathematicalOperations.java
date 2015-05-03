a2463c0b41c94652dbde1018975a11b905209eb8
Can Güler <canguler93@gmail.com>
2015-05-03 20:18:23 +0300

    MathematicalOperations class created, and populated with binary plus operation.

---
+++ src/MathematicalOperations.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Can Guler on 03.05.2015.
+ */
+public class MathematicalOperations {
+    /**
+     * Performs addition of x and y.
+     * Added by Can Guler.
+     * @param x First operand
+     * @param y Second operand
+     * @return Sum of x and y
+     */
+    public static int binaryPlus(int x, int y) {
+        return x+y;
+    }
+}
