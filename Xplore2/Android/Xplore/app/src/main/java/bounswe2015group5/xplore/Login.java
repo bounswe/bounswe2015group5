3eb392acce91e45f9ee6d57c6152579f01557dd5
Hakan Şahin <hakansahin993@gmail.com>
2015-11-05 01:18:24 +0200

    Android / Initial Commit
    Login Screen is designed but it is not functional.

---
+++ Xplore2/Android/Xplore/app/src/main/java/bounswe2015group5/xplore/Login.java
@@ -0,0 +1,50 @@
+package bounswe2015group5.xplore;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * The Class Login is an Activity class that shows the login screen to users.
+ * The current implementation simply start the MainActivity.
+ */
+public class Login extends Activity {
+
+	/* UI references */
+	private EditText edtMail, edtPass;
+	private Button loginBtn;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.login);
+
+		edtMail = (EditText) findViewById(R.id.email);
+		edtPass = (EditText) findViewById(R.id.pass);
+
+		loginBtn = (Button) findViewById(R.id.loginBtn);
+
+		loginBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				attempLogin();
+			}
+		});
+	}
+
+	private void attempLogin() {
+
+		String email = edtMail.getText().toString();
+		String pass = edtPass.getText().toString();
+
+		if(email.equals("admin") && pass.equals("admin")){
+			startActivity(new Intent(getApplication(), MainActivity.class));
+		}
+
+	}
+
+}
---
+++ Xplore2/Android/Xplore/app/src/main/java/bounswe2015group5/xplore/MainActivity.java
@@ -0,0 +1,15 @@
+package bounswe2015group5.xplore;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
