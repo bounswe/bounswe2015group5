d51f290dc7eb8795707bbd883b94f8e6e5cdc11a
Hakan Şahin <hakansahin993@gmail.com>
2015-11-17 13:54:48 +0200

    Rate functionality is added without connecting the backend service

--- Xplore-Android/Xplore/app/src/main/java/bounswe2015group5/xplore/adapters/ContributionListAdapter.java
+++ Xplore-Android/Xplore/app/src/main/java/bounswe2015group5/xplore/adapters/ContributionListAdapter.java
@@ -5,10 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -21,6 +19,7 @@ public class ContributionListAdapter extends BaseAdapter {
 
     private Context context;
     private ArrayList<Contribution> contributions;
+
     public ContributionListAdapter(Context context, ArrayList<Contribution> contributions){
         this.context = context;
         this.contributions = contributions;
@@ -32,7 +31,7 @@ public class ContributionListAdapter extends BaseAdapter {
     }
 
     @Override
-    public Object getItem(int i) {
+    public Contribution getItem(int i) {
         return contributions.get(i);
     }
 
@@ -42,22 +41,56 @@ public class ContributionListAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View convertView, ViewGroup viewGroup) {
+    public View getView(final int i, View convertView, ViewGroup viewGroup) {
 
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) this.context
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.contribution, null);
         }
+
+        Contribution contribution = getItem(i);
+
+        final TextView rateTxt = (TextView) convertView.findViewById(R.id.rate);
+        rateTxt.setText("" + contribution.getRate());
+
         TextView title = (TextView) convertView.findViewById(R.id.cont_detail);
-        title.setText(contributions.get(i).getTitle());
+        title.setText(contribution.getTitle());
 
         TextView nameSurname = (TextView) convertView.findViewById(R.id.cont_name_surname);
-        nameSurname.setText(contributions.get(i).getName() + " " + contributions.get(i).getSurname());
+        nameSurname.setText(contribution.getName() + " " + contribution.getSurname());
 
         TextView date = (TextView) convertView.findViewById(R.id.cont_date);
-        date.setText(contributions.get(i).getDate());
+        date.setText(contribution.getDate());
+
+        ImageButton upVoteBtn = (ImageButton) convertView.findViewById(R.id.up_vote_btn);
+        upVoteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                upDownVote(rateTxt, i, true);
+            }
+        });
+
+        ImageButton downVoteBtn = (ImageButton) convertView.findViewById(R.id.down_vote_btn);
+        downVoteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                upDownVote(rateTxt, i, false);
+            }
+        });
 
         return convertView;
+    }
+
+    public void upDownVote(TextView rateTxt, int i, boolean isUpvoted){
+        Contribution contribution = getItem(i);
+
+        if(contribution.isRated()) return;
+
+        contribution.setRated(true);
+        int rate = contribution.getRate() + (isUpvoted ? 1 : -1);
+
+        rateTxt.setText("" + rate);
+        contribution.updateRate(rate);
     }
 }
--- Xplore-Android/Xplore/app/src/main/java/bounswe2015group5/xplore/models/Contribution.java
+++ Xplore-Android/Xplore/app/src/main/java/bounswe2015group5/xplore/models/Contribution.java
@@ -10,29 +10,35 @@ public class Contribution {
     private String surname;
     private String title;
     private String content;
+    private boolean isRated;
+    private int rate;
 
     /**
      * Default constructor for Contribution. Initializes with null values.
      * @author Mert Oguz
      */
     public Contribution(){
-        date = null;
-        name = null;
-        surname = null;
-        title = null;
-        content = null;
+        this.date = null;
+        this.name = null;
+        this.surname = null;
+        this.title = null;
+        this.content = null;
+        this.isRated = false;
+        this.rate = 0;
     }
 
     /**
      * Constructor with params for Contribution
      * @author Mert Oguz
      */
-    public Contribution(String date, String name, String surname, String title, String content){
+    public Contribution(String date, String name, String surname, String title, String content /* TODO add @isRated and @rate as parameter */){
         this.date = date;
         this.name = name;
         this.surname = surname;
         this.title = title;
         this.content = content;
+        this.isRated = false;
+        this.rate = 0;
     }
 
     /**
@@ -63,9 +69,43 @@ public class Contribution {
 
     /**
      * Access content of contribution
-     * @return
+     * @return @content
      */
     public String getContent(){
         return content;
+    }
+
+    /**
+     * Accesses whether the contribution is rated by the user or not
+     * @return @isRated
+     * @author Hakan Sahin
+     */
+    public boolean isRated() { return isRated; }
+
+    /**
+     * Changes the rate status of the contribution
+     * @author Hakan Sahin
+     */
+    public void setRated(boolean isRated) {
+        this.isRated = isRated;
+        // TODO post this data to the related servlet.
+    }
+
+    /**
+     * Accesses the number of rate of the contribution
+     * @return @rate
+     * @author Hakan Sahin
+     */
+    public int getRate(){
+        return this.rate;
+    }
+
+    /**
+     * Updates the number of rate of the contribution
+     * @author Hakan Sahin
+     */
+    public void updateRate(int rate){
+        // TODO post updated rate to the related servlet.
+        this.rate = rate;
     }
 }
