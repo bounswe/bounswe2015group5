d98f71949e0dd8c76c66c10f4332db5bba962fa0
ulusalomer <ulusalomer@192.168.33.167>
2015-11-30 22:39:15 +0200

    Liking a contribution functionality added.

--- Xplore/src/main/webapp/js/scripts.js
+++ Xplore/src/main/webapp/js/scripts.js
@@ -101,6 +101,12 @@ function bringAllCommentsWithID(contributionID) {
     });
 }
 
+function upvoteContributionWithID(contributionID){
+    $.post("RateContribution", {ContributionID: contributionID, Rate:"1"}, function (responseText) {
+                window.alert(responseText);
+            });
+}
+
 function bringAllContributionsWithTag(TagName) {
     $("#main-div").html("");
     $.get("contribution.html", function (tmpStr) {
@@ -121,6 +127,9 @@ function bringAllContributionsWithTag(TagName) {
                 }
                 tmp.find(".view-comment-button").click({ContributionID: contributions[index].ID}, function (event) {
                     bringAllCommentsWithID(event.data.ContributionID);
+                });
+                tmp.find(".like-button").click({ContributionID: contributions[index].ID}, function (event) {
+                    upvoteContributionWithID(event.data.ContributionID);
                 });
                 $("#main-div").append(tmp);
             }
