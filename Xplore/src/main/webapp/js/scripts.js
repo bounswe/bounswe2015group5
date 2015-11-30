d49a749320195b922472f38c96cbb087a5511d35
Can Güler <canguler93@gmail.com>
2015-11-30 18:06:05 +0200

    Implements tag, contribution, and comment functionalities.

--- Xplore/src/main/webapp/js/scripts.js
+++ Xplore/src/main/webapp/js/scripts.js
@@ -11,11 +11,41 @@ $(document).ready(function () {
         bringContributeForm(event);
     });
 
-    bringAllContributions();
+    $("#logout").click(function (event) {
+        logOut(event);
+    });
+
+    $.post("UserInfo", {}, function (userObj) {
+        if (isLoggedIn(userObj)) {
+            loggedInActions(userObj);
+        } else {
+            loggedOutActions();
+        }
+    });
+
+    bringAllTags();
 });
 
+
+function bringAllTags() {
+    $("#main-div").html("");
+    $.get("tag.html", function (tmpStr) {
+        $.post("AllTags", {}, function (tags) {
+            for (index = 0; index < tags.length; index++) {
+                tmp = $(tmpStr);
+                tmp.find(".tag-name").html(tags[index].TagName);
+                tmp.find(".view-contribution-button").click({TagName: tags[index].TagName}, function (event) {
+                    bringAllContributionsWithTag(event.data.TagName);
+                });
+                $("#main-div").append(tmp);
+            }
+        });
+    });
+}
+
+
 function bringAllContributions() {
-    $("#content").html("");
+    $("#main-div").html("");
     $.get("contribution.html", function (tmpStr) {
         $.post("AllContributions", {}, function (contributions) {
             for (index = 0; index < contributions.length; index++) {
@@ -24,15 +54,69 @@ function bringAllContributions() {
                 tmp.find(".contrib-author").html(contributions[index].Name + " " + contributions[index].Surname);
                 tmp.find(".contrib-date").html(contributions[index].Date);
                 tmp.find(".contrib-content").html(contributions[index].Content);
-                $("#content").append(tmp);
+                $("#main-div").append(tmp);
             }
         });
     });
 }
 
 
+function bringAllCommentsWithID(contributionID) {
+    $("#main-div").html("");
+    doIfLoggedIn(function (userObj) {
+        $("#main-div").load("comment-form.html", function () {
+            $("#submit").click(function (event) {
+                event.preventDefault();
+                var content = $("#content").val();
+                var type = "1";
+                $.post("RegisterComment", {Content: content, ContributionID: contributionID}, function (responseText) {
+                    bringAllCommentsWithID(contributionID);
+                });
+            });
+        });
+    }, function (userObj) {});
+    
+    $.get("comment.html", function (tmpStr) {
+        $.post("CommentsByContributionID", {ContributionID: contributionID}, function (comments) {
+            for (index = 0; index < comments.length; index++) {
+                tmp = $(tmpStr);
+                tmp.find(".comment-author").html(comments[index].Name + " " + comments[index].Surname);
+                tmp.find(".comment-date").html(comments[index].Date);
+                tmp.find(".comment-content").html(comments[index].Content);
+                $("#main-div").append(tmp);
+            }
+        });
+    });
+}
+
+function bringAllContributionsWithTag(TagName) {
+    $("#main-div").html("");
+    $.get("contribution.html", function (tmpStr) {
+        $.post("SearchByTag", {Tag: TagName}, function (contributions) {
+            for (index = 0; index < contributions.length; index++) {
+                tmp = $(tmpStr);
+                tmp.find(".contrib-title").html(contributions[index].Title);
+                tmp.find(".contrib-author").html(contributions[index].Name + " " + contributions[index].Surname);
+                tmp.find(".contrib-date").html(contributions[index].Date);
+                tmp.find(".contrib-content").html(contributions[index].Content);
+                tmp.find(".view-comment-button").click({ContributionID: contributions[index].ID}, function (event) {
+                    bringAllCommentsWithID(event.data.ContributionID);
+                });
+                $("#main-div").append(tmp);
+            }
+        });
+    });
+}
+
+
+function logOut(e) {
+    $.post("Logout", {}, function (userObj) {
+        loggedOutActions();
+    });
+}
+
 function bringSignupForm(e) {
-    $("#content").load("signup-form.html", function () {
+    $("#main-div").load("signup-form.html", function () {
         $("#submit").click(function (event) {
             event.preventDefault();
             var email = $("#email").val();
@@ -41,12 +125,13 @@ function bringSignupForm(e) {
             var password = $("#password").val();
             $.post("RegisterUser", {Email: email, Name: name, Surname: surname, Password: password}, function (responseText) {
                 $.post("UserInfo", {}, function (userObj) {
-                    if (isLoggedIn(userObj)) {
+                    if (!isLoggedIn(userObj)) {
                         window.alert(responseText);
+                        loggedOutActions();
                     } else {
                         window.alert("You're registered successfully!");
-                        displayUserInfo(userObj);
-                        bringAllContributions();
+                        loggedInActions(userObj);
+                        bringAllTags();
                     }
                 });
             });
@@ -56,18 +141,19 @@ function bringSignupForm(e) {
 
 
 function bringLoginForm(e) {
-    $("#content").load("login-form.html", function () {
+    $("#main-div").load("login-form.html", function () {
         $("#submit").click(function (event) {
             event.preventDefault();
             var email = $("#email").val();
             var password = $("#password").val();
             $.post("Login", {Email: email, Password: password}, function (responseText) {
                 $.post("UserInfo", {}, function (userObj) {
-                    if (isLoggedIn(userObj)) {
+                    if (!isLoggedIn(userObj)) {
                         window.alert(responseText);
+                        loggedOutActions();
                     } else {
-                        displayUserInfo(userObj);
-                        bringAllContributions();
+                        loggedInActions(userObj);
+                        bringAllTags();
                     }
                 });
             });
@@ -76,7 +162,7 @@ function bringLoginForm(e) {
 }
 
 function bringContributeForm(e) {
-    $("#content").load("contribute-form.html", function () {
+    $("#main-div").load("contribute-form.html", function () {
         $("#submit").click(function (event) {
             event.preventDefault();
             var title = $("#title").val();
@@ -89,10 +175,36 @@ function bringContributeForm(e) {
     });
 }
 
+function loggedInActions(userObj) {
+    displayUserInfo(userObj);
+    $("#signup").hide();
+    $("#login").hide();
+    $("#contribute").show();
+    $("#logout").show();
+}
+
+function loggedOutActions() {
+    $("#user-info").html("User<strong class=\"caret\"></strong>");
+    $("#signup").show();
+    $("#login").show();
+    $("#contribute").hide();
+    $("#logout").hide();
+}
+
 function displayUserInfo(userObj) {
     $("#user-info").html(userObj.Name + " " + userObj.Surname + "<strong class=\"caret\"></strong>");
 }
 
 function isLoggedIn(userObj) {
-    return (typeof userObj.Email === 'undefined');
+    return (typeof userObj.Email !== 'undefined');
+}
+
+function doIfLoggedIn(thenFunction, elseFunction) {
+    $.post("UserInfo", {}, function (userObj) {
+        if (typeof userObj.Email !== 'undefined') {
+            thenFunction(userObj);
+        } else {
+            elseFunction(userObj);
+        }
+    });
 }
