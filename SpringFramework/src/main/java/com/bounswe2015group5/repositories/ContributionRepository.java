dcea5f680623ff48d041c4342a3aa6298da7af8c
Hanefi <abdullahanefi16@gmail.com>
2015-12-05 14:18:55 +0200

    implemented the many-to-many relationship between tags and contributions. Both api and frontend is done

--- SpringFramework/src/main/java/com/bounswe2015group5/repositories/ContributionRepository.java
+++ SpringFramework/src/main/java/com/bounswe2015group5/repositories/ContributionRepository.java
@@ -2,7 +2,9 @@ package com.bounswe2015group5.repositories;
 
 import com.bounswe2015group5.domain.Contribution;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContributionRepository extends PagingAndSortingRepository<Contribution, Integer> {
 
 }
