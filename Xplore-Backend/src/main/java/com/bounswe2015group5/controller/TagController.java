57b4027c8621ae7881dcbcb09449489fa3b6ed7a
Abdullah Hanefi Önaldı <abdullahanefi16@gmail.com>
2015-12-19 01:24:58 +0200

    Added Relation search support

--- Xplore-Backend/src/main/java/com/bounswe2015group5/controller/TagController.java
+++ Xplore-Backend/src/main/java/com/bounswe2015group5/controller/TagController.java
@@ -2,8 +2,9 @@ package com.bounswe2015group5.controller;
 
 import com.bounswe2015group5.model.Contribution;
 import com.bounswe2015group5.model.Relation;
-import com.bounswe2015group5.repository.ContributionRepo;
+import com.bounswe2015group5.model.Tag;
 import com.bounswe2015group5.repository.RelationRepo;
+import com.bounswe2015group5.repository.TagRepo;
 import org.jsondoc.core.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,38 +16,39 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-
 @RestController
-@RequestMapping(value = "/contributions", produces = MediaType.APPLICATION_JSON_VALUE)
-@Api(description = "The contributions controller", name = "Contribution Services")
-public class ContributionController {
+@RequestMapping(value = "/tags", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(description = "The tags controller", name = "Tag Services")
+public class TagController {
 
     @Autowired
-    ContributionRepo contributionRepo;
+    TagRepo tagRepo;
 
     @Autowired
     RelationRepo relationRepo;
 
-    @ApiMethod(description = "returns one contribution with given id")
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public @ApiResponseObject Contribution findOne(@ApiPathParam(name = "id") @PathVariable int id) {
-        return contributionRepo.findOne(id);
-    }
-
     @ApiMethod
     @RequestMapping(method = RequestMethod.GET)
-    public @ApiResponseObject Iterable<Contribution> findAll() {
-        return contributionRepo.findAll();
+    public @ApiResponseObject Iterable<Tag> findAll() {
+        return tagRepo.findAll();
+    }
+
+    @ApiMethod(description = "returns one tag with given id")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public @ApiResponseObject
+    Tag findOne(@ApiPathParam(name = "id") @PathVariable int id) {
+        return tagRepo.findOne(id);
     }
 
     @ApiMethod
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.CREATED)
-    public @ApiResponseObject ResponseEntity<Void> save(@ApiBodyObject @RequestBody Contribution contribution, UriComponentsBuilder uriComponentsBuilder) {
-        contributionRepo.save(contribution);
+    public @ApiResponseObject
+    ResponseEntity<Void> save(@ApiBodyObject @RequestBody Tag tag, UriComponentsBuilder uriComponentsBuilder) {
+        tagRepo.save(tag);
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(uriComponentsBuilder.path("/contributions/{id}").buildAndExpand(contribution.getId()).toUri());
+        headers.setLocation(uriComponentsBuilder.path("/tags/{id}").buildAndExpand(tag.getId()).toUri());
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
@@ -54,14 +56,14 @@ public class ContributionController {
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     @ResponseStatus(value = HttpStatus.OK)
     public void delete(@ApiPathParam(name = "id") @PathVariable int id) {
-        Contribution contribution = contributionRepo.findOne(id);
-        contributionRepo.delete(contribution);
+        Tag tag = tagRepo.findOne(id);
+        tagRepo.delete(tag);
     }
 
-    @ApiMethod(description = "returns all relations of given contribution id")
+    @ApiMethod(description = "returns all relations of given tag id")
     @RequestMapping(value = "/{id}/relations", method = RequestMethod.GET)
     public @ApiResponseObject
     Page<Relation> findRelationsByTagID(@ApiPathParam(name = "id") @PathVariable int id, Pageable pageable) {
-        return relationRepo.findByContributionId(id,pageable);
+        return relationRepo.findByTagId(id,pageable);
     }
 }
