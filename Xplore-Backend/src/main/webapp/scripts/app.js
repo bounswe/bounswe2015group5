388cf0fa118ecc240082fea8b95b7f984e61f9c0
Can Güler <canguler93@gmail.com>
2015-12-20 01:57:28 +0200

    Implements a simple web frontend skeleton.

---
+++ Xplore-Backend/src/main/webapp/scripts/app.js
@@ -0,0 +1,62 @@
+angular.module('XploreApp', [
+        'XploreAppDep',
+        'ui.router'
+    ])
+    .run(
+        ['$rootScope', '$state', '$stateParams',
+            function ($rootScope, $state, $stateParams) {
+
+                // It's very handy to add references to $state and $stateParams to the $rootScope
+                // so that you can access them from any scope within your applications.For example,
+                // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+                // to active whenever 'contacts.list' or one of its decendents is active.
+                $rootScope.$state = $state;
+                $rootScope.$stateParams = $stateParams;
+            }
+        ]
+    )
+
+    .config(
+        ['$stateProvider', '$urlRouterProvider',
+            function ($stateProvider, $urlRouterProvider) {
+
+                $urlRouterProvider
+
+                    .otherwise('/');
+
+
+                //////////////////////////
+                // State Configurations //
+                //////////////////////////
+
+                // Use $stateProvider to configure your states.
+                $stateProvider
+
+                //////////
+                // Home //
+                //////////
+
+                    .state("home", {
+
+                        // Use a url of "/" to set a state as the "index".
+                        url: "/",
+                        views: {
+                            'graph': {
+                                templateUrl: 'views/graph.html',
+                                controller: 'HomeGraphCtrl'
+                            }
+                        }
+
+                    })
+                    .state("route", {
+                        url: '/route',
+                        views: {
+                            'content': {
+                                template: 'hello'
+                            }
+                        }
+                    })
+            }
+        ]
+    );
+angular.module('XploreAppDep', []);
