dca082b3305163bf6458b0f7396ca333e4ce23a9
Abdullah Hanefi Önaldı <abdullahanefi16@gmail.com>
2015-12-21 14:31:01 +0200

    readied app for deployment

--- Xplore-Backend/src/main/java/com/bounswe2015group5/BackendApplication.java
+++ Xplore-Backend/src/main/java/com/bounswe2015group5/BackendApplication.java
@@ -3,11 +3,18 @@ package com.bounswe2015group5;
 import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 //@Import(ConversionServiceConfiguration.class)
 @SpringBootApplication
 @EnableJSONDoc
-public class BackendApplication {
+public class BackendApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BackendApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
