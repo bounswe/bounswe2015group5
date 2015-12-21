865c47fa38e2f3f06ef7a88524ecf02a0be506bf
Mert Oguz <mert.oguz1@gmail.com>
2015-12-21 14:59:44 +0200

    Profile fragment is created
    First draft of Profile fragment is added

---
+++ Xplore-Android/Xplore/app/src/main/java/bounswe2015group5/xplore/fragments/Profile.java
@@ -0,0 +1,31 @@
+package bounswe2015group5.xplore.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import bounswe2015group5.xplore.R;
+
+/**
+ * Created by Mert on 21.12.2015.
+ */
+public class Profile extends BaseFragment {
+    private LinearLayout contributionList;
+    private ImageView pic;
+    private TextView tvUsername;
+    private TextView tvEmail;
+    private TextView tvTags;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ScrollView parent = (ScrollView) inflater.inflate(R.layout.profile, null);
+
+
+        return parent;
+    }
+}
