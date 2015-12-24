4f6768326b084efb817300210e99c87f3a2fc6aa
bilalyasar <bilalyasar45@gmail.com>
2015-12-24 22:16:24 +0200

    fixed tomcat deploy problem

--- Xplore-Backend/src/main/java/com/bounswe2015group5/BackendApplication.java
+++ Xplore-Backend/src/main/java/com/bounswe2015group5/BackendApplication.java
@@ -1,14 +1,16 @@
 package com.bounswe2015group5;
 
-import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 //@Import(ConversionServiceConfiguration.class)
-@SpringBootApplication
-@EnableJSONDoc
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class BackendApplication extends SpringBootServletInitializer {
 
     @Override
