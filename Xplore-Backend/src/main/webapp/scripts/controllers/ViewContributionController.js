f908cc968da0ab437bd19e485ebe46502af4c575
ulusalomer <ulusalomer@gmail.com>
2016-01-03 21:09:37 +0200

    Created a detail page for contributions.

--- Xplore-Backend/src/main/webapp/scripts/controllers/ViewContributionController.js
+++ Xplore-Backend/src/main/webapp/scripts/controllers/ViewContributionController.js
@@ -1,29 +1,26 @@
 /**
- * Created by burak on 20.12.2015.
+ * Created by ulusalomer on 03.01.2016.
  */
 angular.module('XploreAppDep').controller('ViewContributionCtrl', function ($scope, $http, $state, $stateParams) {
-    $http.get('tags/' + $stateParams.tagId + '/contributions').success(function (data) {
-        $scope.contributions = [];
-        data.forEach(function (contribution) {
-            var tags = [];
-            $http.get('contributions/' + contribution.id + '/tags').success(function (tagsData) {
-                tagsData.forEach(function (tagData) {
-                    tags.push({
-                        id: tagData.id,
-                        name: tagData.name
-                    });
-                });
-            }).then(function () {
-                $scope.contributions.push({
-                    title: contribution.title,
-                    content: contribution.content,
-                    creator: contribution.creator,
-                    creationDate: new Date(contribution.createdAt).toLocaleString(),
-                    tags: tags
+    $scope.contribution;
+    $http.get('contributions/' + $stateParams.contributionId).success(function (contributionData) {
+        var tags= [];
+        $http.get('contributions/' + $stateParams.contributionId + '/tags').success(function (tagsData) {
+            tagsData.forEach(function (tagData) {
+                tags.push({
+                    id: tagData.id,
+                    name: tagData.name
                 });
             });
-
-
+        }).then(function () {
+            $scope.contribution= {
+                id: contributionData.id,
+                title: contributionData.title,
+                content: contributionData.content,
+                creator: contributionData.creator,
+                creationDate: new Date(contributionData.createdAt).toLocaleString(),
+                tags: tags
+            };
         });
     });
 });
