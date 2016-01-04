18d0b3fd6680e3e83016b1bae7c5b5c20df52740
ulusalomer <ulusalomer@gmail.com>
2016-01-04 13:53:09 +0200

    Added the functionality of contributing.

--- Xplore-Backend/src/main/webapp/scripts/controllers/ContributeController.js
+++ Xplore-Backend/src/main/webapp/scripts/controllers/ContributeController.js
@@ -1,10 +1,19 @@
 angular.module('XploreAppDep').controller('ContributeCtrl', function ($scope, $rootScope , $http, $state) {
+    // Add a new contribution
+    // Warning: info of current username should be corrected
+    $scope.contribution = {
+        title: "Enter a title",
+        content: "Enter a content",
+        username: "hanefi"
+    }
+    $scope.contribute = function () {
+        $http.post('contributions', $scope.contribution);
+    }
+
+    /*
     $http.get('tags').success(function(tags){
         $scope.tags = tags;
         $scope.form = {};
         $scope.form.selectedTags = [];
-    });
-
-
-
+    }); */
 });
