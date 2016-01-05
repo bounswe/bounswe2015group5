92ec2df15ce975b7ce1d209c8fc1528d1a6a142d
Can Güler <canguler93@gmail.com>
2016-01-05 22:31:45 +0200

    Adds usage of logged in user when posting a contribution.

--- Xplore-Backend/src/main/webapp/scripts/controllers/ContributeController.js
+++ Xplore-Backend/src/main/webapp/scripts/controllers/ContributeController.js
@@ -1,18 +1,18 @@
-angular.module('XploreAppDep').controller('ContributeCtrl', function ($scope, $rootScope , $http, $state) {
+angular.module('XploreAppDep').controller('ContributeCtrl', function ($scope, $rootScope, $http, $state) {
     // Add a new contribution
     // Warning: info of current username should be corrected
     $scope.contribution = {
         title: "",
         content: "",
-        username: "hanefi"
+        username: $rootScope.username
     }
     $scope.contribute = function () {
         $http.post('contributions', $scope.contribution);
     }
     /*
-    $http.get('tags').success(function(tags){
-        $scope.tags = tags;
-        $scope.form = {};
-        $scope.form.selectedTags = [];
-    }); */
+     $http.get('tags').success(function(tags){
+     $scope.tags = tags;
+     $scope.form = {};
+     $scope.form.selectedTags = [];
+     }); */
 });
