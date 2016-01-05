9826e6bf6356a343458293c226371326ee828be3
ulusalomer <ulusalomer@gmail.com>
2016-01-05 23:53:41 +0200

    Profile Page is created.

--- Xplore-Backend/src/main/webapp/scripts/controllers/ProfileController.js
+++ Xplore-Backend/src/main/webapp/scripts/controllers/ProfileController.js
@@ -1,6 +1,26 @@
 /**
  * Created by burak on 20.12.2015.
  */
-angular.module('XploreAppDep').controller('ProfileCtrl', function ($scope, $rootScope , $http, $state) {
-    // http call to server
+angular.module('XploreAppDep').controller('ProfileCtrl', function ($scope, $rootScope , $http) {
+    $scope.userData;
+    $scope.userContributions = [];
+    $http.get('user/current').success(function(response){
+        $scope.userData = response;
+        $http.get('user/'+response.username+'/contributions').success(function(contributionsData){
+            contributionsData.forEach(function (contribution) {
+                $http.get('contributions/'+contribution.id+'/tags').success(function (tags) {
+                    $scope.userContributions.push({
+                        id: contribution.id,
+                        title: contribution.title,
+                        content: contribution.content,
+                        creator: contribution.creator,
+                        creationDate: new Date(contribution.createdAt).toLocaleString(),
+                        tags: tags
+                    });
+                });
+
+            });
+
+        });
+    });
 });
