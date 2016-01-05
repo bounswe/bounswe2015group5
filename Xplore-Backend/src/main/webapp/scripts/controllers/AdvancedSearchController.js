d06c48fc8162c672000cfff7b757c43dcb0482e6
Can Güler <canguler93@gmail.com>
2016-01-05 17:12:59 +0200

    Implements autocomplete tag selector for advanced search.

--- Xplore-Backend/src/main/webapp/scripts/controllers/AdvancedSearchController.js
+++ Xplore-Backend/src/main/webapp/scripts/controllers/AdvancedSearchController.js
@@ -1,4 +1,14 @@
 angular.module('XploreAppDep').controller('AdvancedSearchCtrl', function ($scope, $http, $state, $httpParamSerializerJQLike) {
+    $scope.searchData = {};
+    $scope.searchData.tags = [];
+    $scope.refreshTags = function() {
+        return $http.get(
+            'tags'
+        ).then(function(response) {
+            $scope.tags = response.data;
+        });
+    };
+
     $scope.advancedSearch = function () {
         var serializedSearchText = $httpParamSerializerJQLike($scope.searchData);
         //console.log(serializedSearchText);
