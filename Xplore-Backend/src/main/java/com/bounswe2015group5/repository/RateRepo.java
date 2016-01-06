316aff7590e969b73281853fa0330342bd27ca02
Abdullah Hanefi Önaldı <abdullahanefi16@gmail.com>
2016-01-06 23:57:42 +0200

    added two more rate search functions

--- Xplore-Backend/src/main/java/com/bounswe2015group5/repository/RateRepo.java
+++ Xplore-Backend/src/main/java/com/bounswe2015group5/repository/RateRepo.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface RateRepo extends CrudRepository<UserRate,UserRate.RateID>{
     List<UserRate> findByContributionId(@Param("id")Integer id);
-//    List<UserRate> findByUserId(@Param("user")String username);
-//    List<UserRate>findByUserIdAndContributionId(@Param("user")String username,@Param("id")Integer id);
+    List<UserRate> findByUser_username(@Param("user")String username);
+    List<UserRate>findByUser_usernameAndContributionId(@Param("user")String username,@Param("id")Integer id);
 }
