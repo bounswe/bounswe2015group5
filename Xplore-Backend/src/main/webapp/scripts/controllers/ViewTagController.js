a2bb4d78ba8f7d2ab70a6874cc03722e3e2d8b94
Can Güler <canguler93@gmail.com>
2016-01-06 02:21:22 +0200

    Implements rate functionality. *  Like or dislike buttons shown to logged in users only *  Like button is disabled if the current user has alredy liked the contribution. (Same for dislike) *  A contribution's rate is displayed. *  Contributions are sorted according to their rates. But API call hasn't activated yet. It uses random rate data instead. They should be activated when the backend the necessary backend API is implemented.

--- Xplore-Backend/src/main/webapp/scripts/controllers/ViewTagController.js
+++ Xplore-Backend/src/main/webapp/scripts/controllers/ViewTagController.js
@@ -2,6 +2,7 @@ angular.module('XploreAppDep').controller('ViewTagCtrl', function ($scope, $http
     $http.get('tags/' + $stateParams.tagId + '/contributions').success(function (data) {
         $scope.contributions = [];
         data.forEach(function (contribution) {
+            contribution.createdAt = new Date(contribution.createdAt).toLocaleString();
             var tags = [];
             $http.get('contributions/' + contribution.id + '/tags').success(function (tagsData) {
                 tagsData.forEach(function (tagData) {
@@ -10,15 +11,32 @@ angular.module('XploreAppDep').controller('ViewTagCtrl', function ($scope, $http
                         name: tagData.name
                     });
                 });
-                $scope.contributions.push({
-                    id: contribution.id,
-                    title: contribution.title,
-                    content: contribution.content,
-                    creator: contribution.creator,
-                    creationDate: new Date(contribution.createdAt).toLocaleString(),
-                    tags: tags
-                });
+                contribution.tags = tags;
+
+
             });
+            //$http.get("contributions/" + contribution.id + "/rates").success(function (ratesData) {
+            //    contribution.rate = ratesData.up - ratesData.down;
+            //    contribution.currentUserVote = ratesData.currentUser;
+            //});
+            var getRandomInt = function (min, max) {
+                return Math.floor(Math.random() * (max - min)) + min;
+            };
+            contribution.rate = getRandomInt(0, 10);
+            contribution.currentUserVote = getRandomInt(-1, 2);
+
+            $scope.contributions.push(contribution);
         });
     });
+
+    $scope.vote = function (contributionId, rate) {
+        var requestPayload = {
+            vote: rate
+        };
+        var requestAddress = "contribution/" + contributionId + "/rates";
+        $http.post(requestAddress, requestPayload).then(function (response) {
+            $state.reload();
+        });
+    };
+
 });
