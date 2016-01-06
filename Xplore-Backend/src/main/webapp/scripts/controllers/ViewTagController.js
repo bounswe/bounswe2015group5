724b39d595f202cbf5cf45d44f0062bc1cdc83f9
Can Güler <canguler93@gmail.com>
2016-01-06 02:54:01 +0200

    Implements delete functionality for contributions.

--- Xplore-Backend/src/main/webapp/scripts/controllers/ViewTagController.js
+++ Xplore-Backend/src/main/webapp/scripts/controllers/ViewTagController.js
@@ -15,15 +15,20 @@ angular.module('XploreAppDep').controller('ViewTagCtrl', function ($scope, $http
 
 
             });
+
+            ////Uncomment these when API is ready
             //$http.get("contributions/" + contribution.id + "/rates").success(function (ratesData) {
             //    contribution.rate = ratesData.up - ratesData.down;
             //    contribution.currentUserVote = ratesData.currentUser;
             //});
+
+            /* Comment out this part when API is ready: START */
             var getRandomInt = function (min, max) {
                 return Math.floor(Math.random() * (max - min)) + min;
             };
             contribution.rate = getRandomInt(0, 10);
             contribution.currentUserVote = getRandomInt(-1, 2);
+            /* Comment out this part when API is ready: END */
 
             $scope.contributions.push(contribution);
         });
@@ -33,10 +38,18 @@ angular.module('XploreAppDep').controller('ViewTagCtrl', function ($scope, $http
         var requestPayload = {
             vote: rate
         };
-        var requestAddress = "contribution/" + contributionId + "/rates";
+        var requestAddress = "contributions/" + contributionId + "/rates";
         $http.post(requestAddress, requestPayload).then(function (response) {
             $state.reload();
         });
+    };
+
+    $scope.delete = function (contributionId) {
+        var requestAddress = "contributions/" + contributionId;
+        console.log("delete to " + requestAddress);
+        //$http.delete(requestAddress).success(function (response) {
+        //    $state.reload();
+        //});
     };
 
 });
