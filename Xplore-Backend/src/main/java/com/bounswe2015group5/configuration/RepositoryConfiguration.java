683ce0af56001ae8ea497e724e95f03fa7ed53d7
melihdemiroren <melih.demiroren@gmail.com>
2016-01-07 15:12:03 +0200

    Javadoc comments added

--- Xplore-Backend/src/main/java/com/bounswe2015group5/configuration/RepositoryConfiguration.java
+++ Xplore-Backend/src/main/java/com/bounswe2015group5/configuration/RepositoryConfiguration.java
@@ -2,6 +2,9 @@ package com.bounswe2015group5.configuration;
 
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * Scans the package of annotated configuration class for Spring Data repositories.
+ */
 @EnableJpaRepositories
 public class RepositoryConfiguration {
 }
